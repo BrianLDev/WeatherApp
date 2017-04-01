b106c64cf771c705e95423709556e421fa23e6b2
Brian Leip <brianleip@gmail.com>
2017-03-31 23:38:15 -0700

    Created main storyboard

--- WeatherApp/WeatherVC.swift
+++ WeatherApp/WeatherVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherVC.swift
 //  WeatherApp
 //
 //  Created by Brian Leip on 3/31/17.
@@ -8,17 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
